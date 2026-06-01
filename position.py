c60b31305416523a24b31c65c8db4d5407ba8ddb
ZIHOUNE Camil <camil.zihoune@eleve.isep.fr>
2026-06-01 13:03:38 +0200

    ajout class position

--- position.py
+++ position.py
@@ -1,11 +1,20 @@
 class Position:
-    def __init__(self, column: str, row: int):
-        self.column = column
-        self.row = row
+    def __init__(self, column, row):
+        self.column = column  # ex: 'e'
+        self.row = row        # ex: 4
 
     def __str__(self):
         return f"{self.column}{self.row}"
 
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.column == other.column and self.row == other.row
+
+
 if __name__ == "__main__":
     p1 = Position("e", 4)
-    print(f"Position créée: {p1}")
+    print(f"Position créée : {p1}")  # Attendu : e4
+    p2 = Position("a", 1)
+    print(f"Position créée : {p2}")  # Attendu : a1
+    print("Tests Position OK !")
