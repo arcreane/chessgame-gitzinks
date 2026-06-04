de228ad5fe3802f5fa2769513d9d618733f3842b
Rayan <ltc.rayan@gmail.com>
2026-06-04 21:58:36 +0200

    fix : test unitaire

--- test_chess.py
+++ test_chess.py
@@ -29,9 +29,9 @@ class TestRook(unittest.TestCase):
     def setUp(self):
         """Plateau vide avec une tour blanche en a1."""
         self.board = Board()
-        self.board.pieces = {}
+        self.board.grid = {}  # On vide le VRAI dictionnaire du plateau !
         self.rook = Rook(0, Position('a', 1))
-        self.board.pieces['a1'] = self.rook
+        self.board.grid['a1'] = self.rook
 
     def test_rook_str(self):
         """La tour doit retourner 'R'."""
@@ -51,17 +51,17 @@ class TestRook(unittest.TestCase):
 
     def test_bloque_par_piece(self):
         """La tour ne peut pas sauter par-dessus une pièce."""
-        self.board.pieces['a3'] = Pawn(0, Position('a', 3))
+        self.board.grid['a3'] = Pawn(0, Position('a', 3))  # <-- Remplacer pieces par grid
         self.assertFalse(self.rook.isValidMove(Position('a', 5), self.board))
 
     def test_capture_ennemi(self):
         """La tour peut capturer une pièce ennemie."""
-        self.board.pieces['a5'] = Pawn(1, Position('a', 5))
+        self.board.grid['a5'] = Pawn(1, Position('a', 5))  # <-- Remplacer pieces par grid
         self.assertTrue(self.rook.isValidMove(Position('a', 5), self.board))
 
     def test_ne_capture_pas_allie(self):
         """La tour ne peut pas capturer un allié."""
-        self.board.pieces['a5'] = Pawn(0, Position('a', 5))
+        self.board.grid['a5'] = Pawn(0, Position('a', 5))  # <-- Remplacer pieces par grid
         self.assertFalse(self.rook.isValidMove(Position('a', 5), self.board))
 
 
